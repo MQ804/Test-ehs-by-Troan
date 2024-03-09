3d8469acd91c7d91dbe20228a9fd7d1bfb9267d0
MQ804 <mq804.developer@gmail.com>
2024-03-09 23:46:02 +0200

    Components. Update common components

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,8 +1,14 @@
+// Login
 export {default as LoginLogo} from './LoginLogo'
 export {default as LoginForm} from './LoginForm'
+
+// Dashboard
 export {default as CardList} from './CardList'
 export {default as CardTarget} from './CardTarget'
 export {default as Diagrams} from './Diagrams'
 export {default as DiagramsCard} from './DiagramsCard'
 export {default as ProgressCircul} from './ProgressCircul'
 export {default as TeacherTopList} from './TeacherTopList'
+
+// Common
+export {default as Calendar} from './Calendar'
